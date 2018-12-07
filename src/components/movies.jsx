cf9f7e9dddfc2a56a6700589d235e906b3abd7c6
Sabuj Ali <prosabuj98@gmail.com>
2018-12-07 16:16:13 +0600

    add like button

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 class movies extends Component {
   state = {
     movies: getMovies()
@@ -8,6 +9,14 @@ class movies extends Component {
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies: movies });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   };
   render() {
     const { length: count } = this.state.movies;
@@ -25,6 +34,7 @@ class movies extends Component {
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
               <th scope="col" />
+              <th scope="col" />
             </tr>
           </thead>
           <tbody>
@@ -34,6 +44,12 @@ class movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
